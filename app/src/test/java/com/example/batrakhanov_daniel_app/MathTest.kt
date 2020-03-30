47334b2940ecc2d09480ba0e0eb1ed70457de83b
dahbka <dahbka96@gmail.com>
2020-03-30 17:51:42 +0500

    increment Added

--- app/src/test/java/com/example/batrakhanov_daniel_app/MathTest.kt
+++ app/src/test/java/com/example/batrakhanov_daniel_app/MathTest.kt
@@ -1,5 +1,13 @@
 package com.example.batrakhanov_daniel_app
 
-class MathTest {
+import org.junit.Assert.assertEquals
+import org.junit.Test
 
+class MathTest {
+ @Test
+ fun incrementTest () {
+     var i=1
+     i+=1
+     assertEquals(2,i)
+ }
 }
